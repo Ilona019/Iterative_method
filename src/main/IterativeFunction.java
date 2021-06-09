6a4244f4e3a6680c325a0fb87b541e35b4119e90
Ilona019 <ilona.arefieva91119@yandex.ru>
2021-06-09 21:31:49 +0300

    my FunctionalInterface CalculateArgument

--- src/main/IterativeFunction.java
+++ src/main/IterativeFunction.java
@@ -11,14 +11,24 @@ public class IterativeFunction {
     public IterativeFunction() {
     }
 
+    @FunctionalInterface
+    public interface CalculateArgument<A, B, C, R> {
+
+        R apply(A a, B b, C c);
+    }
+
+    private static CalculateArgument<Double, Double, Double, Double> argument = (a, x, c) -> {
+            return a / ((x - c) * (x - c));
+    };
+
     //вычисление
     public static double f(double x, double alpha, double betta, double gamma, double delta, double epsilon, double mu) {
         Function<Double, Double> deltaX = z -> z + 0.001;
-        
+
         if (gamma - x == 0 || mu - x == 0) {
             x = deltaX.apply(x);
         }
-        return (alpha * Math.sin(betta / ((x - gamma) * (x - gamma))) + delta * Math.cos(epsilon / ((x - mu) * (x - mu))));
+        return (alpha * Math.sin(argument.apply(betta, x, gamma)) + delta * Math.cos(argument.apply(epsilon, x, mu)));
     }
 
     //вычисление
